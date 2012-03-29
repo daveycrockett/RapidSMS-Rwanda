483c22ed7550dcaf241928d6419fd71562050376
David McCann <david.a.mccann@gmail.com>
2012-03-29 09:38:32 +0200

    backwards-compatibility tweak for locations

--- apps/ubuzima/extensions/locations/location.py
+++ apps/ubuzima/extensions/locations/location.py
@@ -4,5 +4,13 @@ class NameAndCodeLocation(models.Model):
     name = models.CharField(max_length=100, null=False)
     code = models.CharField(max_length=30, null=False)
 
+    @property
+    def latitude(self):
+        return self.point.latitude if self.point else None
+
+    @property
+    def longitude(self):
+        return self.point.longitude if self.point else None
+
     class Meta:
         abstract = True
